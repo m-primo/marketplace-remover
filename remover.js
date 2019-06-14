8f7a5dd6ac79e05598dc6c5c16687342368b18d2
Primo <44984918+m-primo@users.noreply.github.com>
2019-06-14 05:27:56 +0200

    Update remover.js

--- remover.js
+++ remover.js
@@ -1,12 +1,17 @@
 /**
- * Developed By Ayman Abo El Azm
- * Simple Enough ;)
- */
-setInterval(removeMarketPlace, 10);
+    * Developed By Ayman Abo El Azm
+    * Simple Enough ;)
+*/
 
-function removeMarketPlace () {
+// Check every 3 seconds for market place
+// decreasing the number causes more CPU & memory usage.
+setInterval(removeMarketPlace, 3000);
+
+function removeMarketPlace() {
     var marketPlace = document.getElementById('rightCol');
-    if (marketPlace != null) {
+    if(marketPlace != null) {
+        console.log('Market Place detected. Removing...');
         marketPlace.style.visibility = 'hidden';
+        console.log('Market Place has been removed.');
     }
 }
